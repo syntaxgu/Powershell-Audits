d7130abd203b613be59ef1cca7a54db6b41534e1
Syntaxgu <132185497+syntaxgu@users.noreply.github.com>
2023-04-30 09:54:05 -0600

    Update WIndows Baseline Script.ps1

--- WIndows Baseline Script.ps1
+++ WIndows Baseline Script.ps1
@@ -1,5 +1,5 @@
 ﻿#Define your remote target(s) here
-$Remote = "<remotehost1>", "<remotehost2>"
+$Remotehost = "<remotehost1>", "<remotehost2>"
 
 #Tailor your commands to your output desires
 $Cmd = {
@@ -17,9 +17,9 @@ $Cmd = {
 #Displays a login portal with your username 
 $Cred = Get-Credential "<username>"
 
-#Loops in every device define in your $Rmtebox variable (You may also comment out this line if your situation call for one box instead of multiple)
-foreach ($host in $Remote){
-#Invoke command allows us to remote into target device and rund the $cmd variable on the remote machine
+#Loops through every device define in your $Remotehost variable (You may also comment out this line if your situation call for one box instead of multiple)
+foreach ($host in $Remotehost){
+#Invoke command allows us to run the $cmd variable on the remote device 
     Write-host $box
         Invoke-Command -ComputerName -Credential $Cred -ScriptBlock { $Cmd
         }
